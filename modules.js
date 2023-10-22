79c7e7bb6e6f3985a15025d3bcb9ffafad881506
Ryan <103019033+ryaneditraj@users.noreply.github.com>
2023-10-22 08:43:20 +0530

    Initial commit

---
+++ modules.js
@@ -0,0 +1,14 @@
+const d =document;
+import hamburgerMenu from "./controllers/hamburger.js";
+import btnDarkMode from "./controllers/electricSound.js";
+import toggleDarkMode from "./controllers/darkTheme.js";
+import {toTop} from "./controllers/toTop.js";
+import inputs from  "./controllers/form.js";
+import generarCards from "./components/CardSkills.js";
+import CardsPortafolio from "./components/CardsPortafolio.js";
+
+
+
+
+CardsPortafolio();
+
